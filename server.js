debac4d15df0787e649dafba938c36a343aeca57
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-06-26 16:20:41 +1000

    finished project

--- server.js
+++ server.js
@@ -2,11 +2,46 @@ const express = require("express");
 const app = express();
 
 const { quotes } = require("./data");
-const { getRandomElement } = require("./utils");
+const { getRandomElement, getElementsBasedOffKeyAndValue } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static("public"));
+
+app.get("/api/quotes/", (req, res, next) => {
+    if (Object.keys(req.query).length === 0) {
+        res.send(quotes);
+        return;
+    }
+
+    const person = req.query.person;
+    const arrayItems = getElementsBasedOffKeyAndValue(quotes, "person", person);
+
+    res.send(arrayItems);
+});
+
+app.get("/api/quotes/random", (req, res, next) => {
+    const randomQuote = getRandomElement(quotes);
+
+    res.json({ quote: randomQuote });
+});
+
+app.post("/api/quotes", (req, res, next) => {
+    const newQuote = req.query;
+
+    if (
+        Object.keys(req.query).length === 0 ||
+        !newQuote.quote ||
+        !newQuote.person
+    ) {
+        res.status(400).send();
+        return;
+    }
+
+    quotes.push(newQuote);
+
+    res.status(201).send(newQuote);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
--- utils.js
+++ utils.js
@@ -1,8 +1,14 @@
-const getRandomElement = arr => {
-  if (!Array.isArray(arr)) throw new Error('Expected an array');
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+const getRandomElement = (arr) => {
+    if (!Array.isArray(arr)) throw new Error("Expected an array");
+    return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const getElementsBasedOffKeyAndValue = (arr, key, value) => {
+    const filteredArray = arr.filter((item) => item[key] === value);
+    return filteredArray;
+};
 
 module.exports = {
-  getRandomElement
+    getRandomElement,
+    getElementsBasedOffKeyAndValue,
 };
